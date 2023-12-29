365e675d9783d993f829bfe8082b34a6fbae6a8f
binaydubey <63951198+binaydubey@users.noreply.github.com>
2023-12-29 23:54:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Current Working Directory</title>
+</head>
+<body>
+
+<script>
+    // Display the current URL
+    var currentURL = window.location.href;
+    alert("Current URL: " + currentURL);
+</script>
+
+</body>
+</html>
